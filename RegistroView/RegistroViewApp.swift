68d370c527c83deee575a58532141a935c8f1faf
Marco <empleomarcotapia@gmail.com>
2024-04-18 06:39:33 -0700

    Initial Commit

---
+++ RegistroView/RegistroViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistroViewApp.swift
+//  RegistroView
+//
+//  Created by Marco on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct RegistroViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
